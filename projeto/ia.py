193a482eac4658fd4df70d15680e88903f6686f8
Jason <guilherme.acra@usp.br>
2017-09-18 22:30:40 -0300

    Mudanças na IA

--- projeto/ia.py
+++ projeto/ia.py
@@ -9,19 +9,37 @@ class IA(Thread):
         self.distSonar = None
         self.mundo = mundo
         self.movimentacao = Movimentacao()
+        self.flag = False
+        self.flag2 = False
         Thread.__init__(self)
 
     def decisao(self):
         if self.mundo.atualizado == True:
             self.listaBexiga = self.mundo.listaBexiga
             bexiga = self.listaBexiga.get('orange')
+            if self.flag2 == True:
+                if bexiga.visivel == False:
+                    self.movimentacao.mover(-14, 0)
+                    #sleep(100)
+                else:
+                    self.flag2 = False
+                return
+            
             if bexiga.visivel == True:
+                if self.flag == True:
+                    self.flag2 = True
+                    self.flag = False
+                    return
                 (x, y) = bexiga.getPos()
                 velAng = int(x*100)
                 velLim = int((1-bexiga.altura)*100)
-                self.movimentacao.mover(velAng, velLim)
+                if velLim < 15 and velLim != 0:
+                    velLim = 15
+                    velAng = velAng * 1.5
+                self.movimentacao.mover(-int(velAng/2.5), int(velLim))
             else:
-                self.movimentacao.mover(0, 0)
+                self.flag = True
+                self.movimentacao.mover(20, 0)
             self.mundo.atualizado = False
 
     def run(self):
